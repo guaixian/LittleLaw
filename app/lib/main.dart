e5ab064aea69e853ddbbd717538c3e7bf7dd1ab5
yangxiong <yangxiong@aiguo.com>
2026-09-14 12:06:47 +0800

    release: v2.2.0

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1288,7 +1288,7 @@ class _ProfilePageState extends State<ProfilePage> {
               ListTile(
                 leading: const Icon(Icons.info_outline),
                 title: const Text('关于 LittleLaw'),
-                subtitle: const Text('v2.1.0 · NoServer 架构 · 协议 v1'),
+                subtitle: const Text('v2.2.0 · NoServer 架构 · 协议 v1'),
                 onTap: () {},
               ),
             ],
